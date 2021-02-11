c270ea10c4dc25fa02441fd85512c432940ff8c2
Ahmad Asmandar <ahmedasmndr2@gmail.com>
2021-02-12 00:15:49 +0100

    add combo ready to exe

--- lunch.py
+++ lunch.py
@@ -2,10 +2,153 @@ import serial.tools.list_ports
 from PyQt5 import QtWidgets, uic, QtSerialPort, QtCore, QtGui
 import os
 import sys
-from PyQt5.QtWidgets import QFileDialog, QMessageBox
-from Cleaner import Cleaner
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QComboBox
+
+# from Cleaner import Cleaner
 import traceback
 import signal
+
+import os
+import re
+import pandas as pd
+
+
+class Cleaner:
+    def __init__(self):
+
+        self.names = None
+
+    def getFolderContent(self, dir=None):
+        if not dir:
+            self.FolderContent = os.listdir()
+        else:
+            self.FolderContent = os.listdir(os.path.dirname(dir))
+        return self.FolderContent
+
+    def cleanTxtfiles(self, dir=None):
+
+        if not dir:
+            return
+        else:
+            if not os.path.exists("./cleandata"):
+                os.makedirs("./cleandata")
+                os.makedirs("./excels")
+            self.FolderContent = os.listdir(dir)
+
+            for item in self.FolderContent:
+                # delete the text files and the urls files to clean the library
+                # print(item)
+                if item.find(".txt") > 0 and item != ".git":
+                    # print(item)
+                    with open("./{}".format(item), "r") as f:
+                        lines = f.readlines()
+                        f.close()
+                    #     # if (os.path.exists("./cleandata/{}".format(item))):
+                    with open("./cleandata/{}".format(item), "w+") as f:
+                        for line in lines:
+                            if "10 Messungen" in line.strip("\n"):
+                                line = line[line.find("Ch") :]
+                                # print(newline)
+                            # if (line.strip("\n") != "þStartfischer1 p33-6 hf8 fc6x 0.9km")
+                            if "Ch" in line.strip("\n") or "V:" in line.strip("\n"):
+                                f.write(line.replace(";", ""))
+
+    def exportExcel(self):
+
+        a = {}
+        lst = {}
+        content = os.listdir("./cleandata")
+        result = pd.DataFrame()
+        verhaeltnis = pd.DataFrame()
+        deltas = pd.DataFrame()
+        new_df = pd.DataFrame()
+        for x in content:
+            if x.find(".txt") > 0 and x != ".git":
+                # print(x)
+                df = pd.read_csv(
+                    "./cleandata/{}".format(x),
+                    names=["channal", "min", "max", "x", "y"],
+                    sep="\s+",
+                    encoding="ISO-8859-1",
+                )
+                dx = df.drop(["x", "y"], axis=1)
+                # .map(lambda x: x.lstrip("+-;").rstrip("aAbBcC;"))
+                dx["min"] = dx["min"]
+                # .map(lambda x: x.lstrip(";").rstrip(";"))
+                dx["channal"] = dx["channal"]
+                if 100 in dx.index:
+                    dx = dx.drop(100)
+                dx["min"] = pd.to_numeric(dx["min"])
+                dx["max"] = pd.to_numeric(dx["max"])
+                dx["deltas"] = dx["max"] - dx["min"]
+                # dx["name"]=x.strip('.txt')
+                ########### Extract channals ######
+
+                ch0 = dx[dx.channal == "Ch0:"]
+                ch1 = dx[dx.channal == "Ch1:"]
+                ch2 = dx[dx.channal == "Ch2:"]
+                ch3 = dx[dx.channal == "Ch3:"]
+                ch4 = dx[dx.channal == "Ch4:"]
+                ch5 = dx[dx.channal == "Ch5:"]
+                ch6 = dx[dx.channal == "Ch6:"]
+                ch7 = dx[dx.channal == "Ch7:"]
+                ch8 = dx[dx.channal == "Ch8:"]
+                # print(ch0.head())
+                ############ build the deltas #############
+                delta0 = ch0["max"] - ch0["min"]
+                lst["delta0"] = delta0
+                delta1 = ch1["max"] - ch1["min"]
+                lst["delta1"] = delta1
+                delta2 = ch2["max"] - ch2["min"]
+                lst["delta2"] = delta2
+                delta3 = ch3["max"] - ch3["min"]
+                lst["delta3"] = delta3
+                delta4 = ch4["max"] - ch4["min"]
+                lst["delta4"] = delta4
+                delta5 = ch5["max"] - ch5["min"]
+                lst["delta5"] = delta5
+                delta6 = ch6["max"] - ch6["min"]
+                lst["delta6"] = delta6
+                delta7 = ch7["max"] - ch7["min"]
+                lst["delta7"] = delta7
+                delta8 = ch8["max"] - ch8["min"]
+                lst["delta8"] = delta8
+                # print(delta0.head())
+                # format reset index
+
+                df_new = pd.DataFrame.from_dict(lst)
+                df_new = df_new.apply(lambda x: pd.Series(x.dropna().values))
+                # print(df_new.head(3))
+                dv = dx[dx.channal == "V:"]
+
+                # connect the name of Measurement
+                dv.index.name = x.strip(".txt")
+                dx.index.name = x.strip(".txt")
+                df_new.index.name = x.strip(".txt")
+
+                dx = dx.reset_index()
+                dv = dv.reset_index()
+                df_new = df_new.reset_index()
+
+                if not (dx.empty):
+                    a[x.strip(".txt")] = dx
+                dv = dv.replace("V:", "V:{}".format(x.strip(".txt")))
+                rows = df_new.shape[0]
+                # print(rows)
+                for row in range(rows):
+                    df_new["name"] = x.strip(".txt")
+                # drop empty Dataframes
+                if not (dx.empty):
+                    if not os.path.exists("./excels"):
+                        # os.makedirs("./cleandata")
+                        os.makedirs("./excels")
+                    result = pd.concat([result, dx.append(pd.Series(name="Verh..", dtype="float"))], axis=1)
+                    verhaeltnis = pd.concat([verhaeltnis, dv], axis=1)
+                    new_df = result.append(verhaeltnis)
+                    deltas = pd.concat([deltas, df_new], axis=1)
+                    dv.to_excel("./excels/{}-verhaeltnis.xlsx".format(x.strip(".txt")))
+                    dx.to_excel("./excels/{}-result.xlsx".format(x.strip(".txt")))
+                    df_new.to_excel("./excels/{}-deltas.xlsx".format(x.strip(".txt")))
 
 
 class test_lunch(QtWidgets.QMainWindow):
@@ -57,6 +200,12 @@ class test_lunch(QtWidgets.QMainWindow):
         self.exc_com.clicked.connect(self.excute)
         self.restart_bu.clicked.connect(self.restart)
         self.clear_bu.clicked.connect(self.clear)
+
+        ##ComboBox
+        self.comboBox.addItem(" ")
+        self.comboBox.addItem("2.5S")
+        self.comboBox.addItem("3S")
+        self.comboBox.addItem("5S")
 
     ##########################################################################
     def excute(self):
@@ -127,12 +276,24 @@ class test_lunch(QtWidgets.QMainWindow):
             else:
                 self.terminal.setStyleSheet("color: black ")
                 self.message_le = self.data_send.text()
-                if "Send" in self.message_le:
-                    self.terminal.append("\n")
-                    self.serial.write("\n".encode())
+                self.checkcombo = self.comboBox.currentText()
+
+                if self.checkcombo == " ":
+
+                    if "Send" in self.message_le:
+                        self.terminal.append("\n")
+                        self.serial.write("\n".encode())
+                    else:
+                        self.terminal.append(self.message_le)
+                        self.serial.write(self.message_le.encode())
                 else:
-                    self.terminal.append(self.message_le)
-                    self.serial.write(self.message_le.encode())
+                    if self.checkcombo == "2.5S":
+                        self.serial.write("a".encode())
+                    elif self.checkcombo == "3S":
+                        self.serial.write("b".encode())
+
+                    elif self.checkcombo == "5S":
+                        self.serial.write("c".encode())
 
         except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -188,8 +349,8 @@ class test_lunch(QtWidgets.QMainWindow):
                 QMessageBox.Yes | QMessageBox.No,
                 QMessageBox.No,
             )
-            
-            #self.message_7.setFont(self, self.font)
+
+            # self.message_7.setFont(self, self.font)
             # .setStyleSheet("QPushButton{border-radius: 1px; /* Green */color: white;}")
 
             if self.message_7 == QMessageBox.Yes:
