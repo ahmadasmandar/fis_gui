8dcd42e24d4960a9ca8537b8a05e58aa27db7f1a
Ahmad Asmandar <ahmedasmndr2@gmail.com>
2021-02-16 23:11:14 +0100

    try to test subprocess

--- open.py
+++ open.py
@@ -18,23 +18,26 @@ missing = required - installed
 check = 0
 
 
-def install(package):
-    check = os.system("start /wait   cmd  /c pip install {}".format(package))
-    print(subprocess.Popen("python lunch.cpython-38.pyc", shell=True, stdout=subprocess.PIPE).stdout.read())
-    # subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-    return check
+# def install(package):
+#     check = os.system("start /wait   cmd  /c pip install {}".format(package))
+#     print(subprocess.Popen("python lunch.cpython-38.pyc", shell=True, stdout=subprocess.PIPE).stdout.read())
+#     # subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+#     return check
 
 
-if missing:
-    print("hallo")
-    for x in missing:
-        check = install(x)
-        while check != 0:
-            time.sleep(0.1)
+# if missing:
+#     print("hallo")
+#     for x in missing:
+#         check = install(x)
+#         while check != 0:
+#             time.sleep(0.1)
 
-check = subprocess.Popen("python lunch.cpython-38.pyc", shell=False)
-print(check)
+# check = subprocess.Popen("python lunch.cpython-38.pyc", shell=False)
+# print(check)
 # os.system("cmd /c python lunch.cpython-38.pyc")
 # subprocess.run(["python lunch.cpython-38.pyc"])
 # subprocess.Popen("python lunch.cpython-38.pyc", shell=True)
 # pid = subprocess.Popen([sys.executable, "lunch.cpython-38.pyc"], shell=True)
+result = subprocess.run([sys.executable,"lunch.cpython-38.pyc"], capture_output=True, text=True)
+print(result.stdout)
+print(result.stderr)
